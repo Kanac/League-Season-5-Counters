834ac622a3ed4b10c63f30d500c3712c08c330c0
Anthony C <broac@live.ca>
2015-06-28 07:40:31 -0700

    AdError

---
+++ League of Legends Counterpicks.Shared/Common/AdError.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AdMediator.WindowsPhone81;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace League_of_Legends_Counterpicks.Common
+{
+    public class AdError
+    {
+        public void Ad_Error(object sender, Microsoft.AdMediator.Core.Events.AdMediatorFailedEventArgs e)
+        {
+            var adName = (sender as AdMediatorControl).Id;
+            Debug.WriteLine("AdMediatorError for " + adName + ":" + e.Error + " " + e.ErrorCode);
+        }
+    }
+}
