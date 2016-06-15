a1019b1aaab2189612522a84774cd7043f902448
Anthony C <broac@live.ca>
2016-06-14 21:39:39 -0700

    update new patch

--- League of Legends Counterpicks/RolePage.xaml.cs
+++ League of Legends Counterpicks/RolePage.xaml.cs
@@ -359,7 +359,7 @@ namespace League_of_Legends_Counterpicks
             {
                 AdControl ad = new AdControl();
                 ad.ApplicationId = "bf747944-c75c-4f2a-a027-7c159b32261d";
-                ad.AdUnitId = "300199";
+                ad.AdUnitId = "249556";
                 ad.Style = Application.Current.Resources["HorizontalAdSmall"] as Style;
                 ad.IsAutoRefreshEnabled = false;
                 ad.Refresh();
